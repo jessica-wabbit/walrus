36f093ce5a3c4af25042a9e507a4a1447e2ee7f2
Jessica Robertson <jlynnr90@gmail.com>
2017-02-06 19:33:25 -0800

    Add new images and apply new styles to CSS and effects to the index.html file by changing the scripts.js functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
-  $("h1").click(function() {
-    alert("This is a header.");
+
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
 });
